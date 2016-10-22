60e4977b176e6edbea318168fb4411e84b8b3d21
Avik Das <avik.das@berkeley.edu>
2016-10-21 23:46:05 -0700

    Add ability to send messages
    This demonstrates how a main loop would be set up, and how to send
    messages. The next step is to integrate listening to messages in the
    main loop, instead of having a prompt.

--- __main__.py
+++ __main__.py
@@ -60,20 +60,58 @@ def conversation_name(conv):
     return ', '.join(u.full_name for u in users)
 
 
-def on_connect_handler(client):
+def prompt_and_send_callback(loop, client, conversations):
+    def prompt_and_send():
+        """
+        Prompt for a conversation and a message, then send the message to that
+        conversation.
+        """
+
+        conv_prompt = \
+            'Which conversation? [0-{}]'.format(len(conversations) - 1)
+        print(conv_prompt, end=' ')
+        conv_index = int(input())
+
+        print('Message:', end=' ')
+        message = input()
+
+        segments = hangups.ChatMessageSegment.from_str(
+            '[FROM HANGBOT] {}'.format(message))
+        conversations[conv_index].send_message(segments)
+        (asyncio.async(conversations[conv_index].send_message(segments))
+            .add_done_callback(on_message_sent))
+
+    def on_message_sent(future):
+        """Handle showing an error if a message fails to send."""
+
+        try:
+            future.result()
+
+            print()
+            loop.call_soon(prompt_and_send)
+        except hangups.NetworkError:
+            print('Failed to send message')
+
+    return prompt_and_send
+
+
+def on_connect_handler(loop, client):
     @asyncio.coroutine
     def on_connect():
         """Handle connecting for the first time."""
+
         users, convs = \
             yield from hangups.build_user_conversation_list(client)
 
-        convs = reversed(
-            sorted(convs.get_all(), key=lambda c: c.last_modified))
+        convs = list(reversed(
+            sorted(convs.get_all(), key=lambda c: c.last_modified)))
 
-        print('USER LIST:')
-        print(users)
-        print('CONV LIST:')
-        print([conversation_name(c) for c in convs])
+        print('Conversations:')
+        for i, c in enumerate(convs):
+            print('  {:>3}. {}'.format(i, conversation_name(c)))
+        print()
+
+        loop.call_soon(prompt_and_send_callback(loop, client, convs))
 
     return on_connect
 
@@ -84,11 +122,12 @@ if __name__ == '__main__':
     except hangups.GoogleAuthError as e:
         sys.exit('Login failed ({})'.format(e))
 
-    client = hangups.Client(cookies)
-    client.on_connect.add_observer(on_connect_handler(client))
-
     try:
         loop = asyncio.get_event_loop()
+
+        client = hangups.Client(cookies)
+        client.on_connect.add_observer(on_connect_handler(loop, client))
+
         loop.run_until_complete(client.connect())
     finally:
         loop.close()
