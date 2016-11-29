7b185780aa774c4547886bbf55e73a62f4a834c7
Avik Das <avik.das@berkeley.edu>
2016-11-28 21:57:33 -0800

    Fix #5: set up scripts architecture
    In doing so, I made use of Python 3.5's `async`/`await` functionality,
    which allowed me to refactor some of the existing code to be
    self-consistent.  To add new scripts, the steps are:
    
    - Add a new file in the `scripts/` directory. This file should have an
      `async` function named `process`. The function should return `True` if
      the message was processed and `False` otherwise.
    - Import this module in `__main__.py` and add it to the
      `ENABLED_SCRIPTS` array.
    
    As part of this change, I added the first script to send random Ron
    Swanson quotes.

--- __main__.py
+++ __main__.py
@@ -6,6 +6,8 @@ import sys
 
 import hangups
 
+import scripts.swanson
+
 
 logger = logging.getLogger('hangbot')
 logger.setLevel(logging.DEBUG)  # this could depend on the environment
@@ -29,6 +31,11 @@ file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(
     logging.Formatter('%(asctime)s - %(name)s - [%(levelname)s] %(message)s'))
 logger.addHandler(file_handler)
+
+
+ENABLED_SCRIPTS = [
+    scripts.swanson,
+]
 
 
 def get_auth_env():
@@ -86,38 +93,29 @@ def conversation_name(conv):
     return ', '.join(u.full_name for u in users)
 
 
-def receive_message_callback(message_event, client, conversation):
-    def receive_message():
-        """
-        For a chat message event, send a response reflecting the same message.
-        """
+async def receive_message(message_event, client, conversation):
+    """
+    For a chat message event, go through the enabled scripts until one of
+    them processes a message.
+    """
 
-        segments = hangups.ChatMessageSegment.from_str(
-            '[FROM HANGBOT] {}'.format(message_event.text))
-        conversation.send_message(segments)
-        (asyncio.async(conversation.send_message(segments))
-            .add_done_callback(on_message_sent))
-
-    def on_message_sent(future):
-        """Handle showing an error if a message fails to send."""
-
-        try:
-            future.result()
-        except hangups.NetworkError as e:
-            logger.exception('Failed to send message: %s', e)
-
-    return receive_message
+    try:
+        for script in ENABLED_SCRIPTS:
+            if await script.process(client, conversation, message_event):
+                break
+    except Exception as e:
+        logger.exception('Failed to send process message: %s', e)
 
 
 def on_event_handler(loop, client, conversation):
-    def on_event(event):
+    async def on_event(event):
         """Handles the last chat message from the client."""
 
         if isinstance(event, hangups.ChatMessageEvent):
             if not conversation.get_user(event.user_id).is_self:
                 logger.info('Processing message: %s', event.text)
-                loop.call_soon(
-                    receive_message_callback(
+                asyncio.ensure_future(
+                    receive_message(
                         event,
                         client,
                         conversation))
@@ -126,12 +124,10 @@ def on_event_handler(loop, client, conversation):
 
 
 def on_connect_handler(loop, client):
-    @asyncio.coroutine
-    def on_connect():
+    async def on_connect():
         """Handle connecting for the first time."""
 
-        users, convs = \
-            yield from hangups.build_user_conversation_list(client)
+        users, convs = await hangups.build_user_conversation_list(client)
 
         convs = list(reversed(
             sorted(convs.get_all(), key=lambda c: c.last_modified)))
